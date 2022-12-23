5ed118d60ea77b47b79e9503e10ab884cc27627c
majdi aribi <majdi.developer@gmail.com>
2022-12-23 20:48:36 +0100

    finish first step in backend social media :boom

--- api/index.js
+++ api/index.js
@@ -33,6 +33,7 @@ app.use("/api/users",userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts",postRoute);
 
+
 app.listen(8800,()=>{
     console.log("backend is running with majdouch ");
 
